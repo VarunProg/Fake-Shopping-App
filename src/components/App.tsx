e1713e35fa0f8a7c0fc3d737e66edb3c34a512fe
VarunProg <89865506+VarunProg@users.noreply.github.com>
2022-03-03 20:49:39 +0100

    send state data as a prop to Iproduct component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,39 +1,44 @@
-import { useEffect, useState } from 'react'
-import { worker } from '../mocks/Browser'
-
-import '../styles/App.css'
+import { useEffect, useState } from "react";
+import { worker } from "../mocks/Browser";
+import "../styles/App.css";
+import ProductItem from "./ProductItem";
 // started mock server
 worker.start();
 const App = () => {
-   
- // defined type for products
- interface Iproduct{
-   name:string;
-   price: number;
-  rating:number;
-  catergory:string;
-  image: string;
-  id:string
- }
-   const [products, setProducts] =useState<Iproduct[]>([]);
-   
-  //fetching data from mock server Api
-  const getProducts = async () =>{
-     const res = await fetch ('/products');
-     const json = await res.json()
-     console.log(json.items)
-     setProducts(json.items);
+  // defined type for products
+  interface Iproduct {
+    name: string;
+    price: number;
+    rating: number;
+    catergory: string;
+    image: string;
+    id: string;
   }
-  useEffect( ()=>{
-   // call getUsers function
-   getProducts();
-  },[])
+  //set interface to state
+  const [products, setProducts] = useState<Iproduct[]>([]);
+
+  //fetching data from mock server Api
+  const getProducts = async () => {
+    const res = await fetch("/products");
+    const json = await res.json();
+    console.log(json.items);
+    setProducts(json.items);
+  };
+  useEffect(() => {
+    // call getUsers function
+    getProducts();
+  }, []);
   return (
-   <>
+    <>
       <h1>Hi Varun</h1>
-   </>
-  )
-}
+      {/* sending state as a prop */}
+      <div className="container">
+        {products.map((product) => (
+          <ProductItem key={product.id} product={product} />
+        ))}
+      </div>
+    </>
+  );
+};
 
 export default App;
-
