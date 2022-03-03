00543712e9d462e162b52dd3e30ece6224a8727d
VarunProg <89865506+VarunProg@users.noreply.github.com>
2022-03-03 07:37:58 +0100

    fetching data from mock server Api

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { worker } from '../mocks/Browser'
 
 import '../styles/App.css'
@@ -6,6 +6,16 @@ import '../styles/App.css'
 worker.start();
 function App() {
 
+  //fetching data from mock server Api
+  const getUsers = async () =>{
+     const res = await fetch ('/user');
+     const data = await res.json()
+     console.log(data)
+  }
+  useEffect( ()=>{
+   // call getUsers function
+   getUsers()
+  },[])
   return (
    <>
 
--- src/mocks/Handlers.ts
+++ src/mocks/Handlers.ts
@@ -3,5 +3,13 @@ export const handlers = [
   // Handles a POST /login request
   rest.post('/login', null),
   // Handles a GET /user request
-  rest.get('/user', null),
+  rest.get('/user', (req, res, ctx) => {
+     return res(
+      // Respond with a 200 status code
+      ctx.status(200),
+      ctx.json({
+          name: 'Varun',
+        })
+    )
+  }),
 ]
