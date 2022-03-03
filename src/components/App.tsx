b7c1f3c405c391e000170d4fa7b43e0b52e6fcf8
VarunProg <89865506+VarunProg@users.noreply.github.com>
2022-03-03 07:18:56 +0100

    started mock server

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,14 @@
 import { useState } from 'react'
+import { worker } from '../mocks/Browser'
+
 import '../styles/App.css'
 
+worker.start();
 function App() {
 
   return (
    <>
-   
+
    </>
   )
 }
