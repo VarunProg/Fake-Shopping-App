e355d6f74e59aa80bf606bf363b90bfe2ae3372b
VarunProg <89865506+VarunProg@users.noreply.github.com>
2022-04-14 17:58:42 +0200

    revert set ":construction: use sets in stta"
    This reverts commit 8f85ceb871b500d4425d6c44d1eea5ddeffdd6be.

--- src/Store/cartSlice.ts
+++ src/Store/cartSlice.ts
@@ -7,13 +7,13 @@ enableMapSet();
 export interface CartState {
   totalItems: number;
   items: Iproduct[];
-  ids: Set<Iproduct["id"]>;
+  ids: Iproduct["id"][];
 }
 
 const initialState: CartState = {
   totalItems: 0,
   items: [],
-  ids: new Set(),
+  ids: [],
 };
 
 export const cartSlice = createSlice({
@@ -21,23 +21,19 @@ export const cartSlice = createSlice({
   initialState,
   reducers: {
     addProduct: (state, action: PayloadAction<Iproduct>) => {
-      // const ids = new Set([...state.ids]); // first time empty
-
-      const isProductAlreadyInCart = state.ids.has(action.payload.id); // checking id
+      const ids = new Set([...state.ids]); // first time empty
+      const isProductAlreadyInCart = ids.has(action.payload.id); // checking id
 
       if (!isProductAlreadyInCart) {
-        return {
-          ids: new Set([...state.ids, action.payload.id]),
-          totalItems: state.totalItems + 1,
-          items: [...state.items, action.payload],
-        };
+        state.items.push(action.payload);
+        state.ids.push(action.payload.id);
+        state.totalItems += 1;
       }
-      return state;
     },
     removeProduct: (state, action: PayloadAction<{ id: string }>) => {
       console.log(action.payload.id);
       // state.items = state.items.filter((item) => item.id !== action.payload.id);
-      state.totalItems -= 1; // remove items
+      state.totalItems -= 1; // remove items from state
     },
   },
 });
